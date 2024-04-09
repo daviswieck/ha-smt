b14717113ce93e5dfaab5f3686d2ec1d9c96206b
gopher_dave <79770840+daviswieck@users.noreply.github.com>
2024-04-08 23:37:43 -0500

    Update __init__.py

--- custom_components/smt/__init__.py
+++ custom_components/smt/__init__.py
@@ -1,22 +1,22 @@
-"""The SmartMeterTexas integration."""
-import logging
-
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 
-DOMAIN = "smt"
+DOMAIN = "ha_smt"
 
 async def async_setup(hass: HomeAssistant, config: dict):
-    """Set up the SmartMeterTexas component."""
+    """Set up the Smart Meter Texas integration."""
+    # No configuration necessary for this type of integration
     return True
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
-    """Set up SmartMeterTexas from a config entry."""
-    # Add setup code here, if necessary
+    """Set up Smart Meter Texas from a config entry."""
+    # Perform setup for the configured entry (e.g., create entities)
+    hass.async_create_task(
+        hass.config_entries.async_forward_entry_setup(entry, "sensor")
+    )
     return True
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
     """Unload a config entry."""
-    # Add cleanup code here, if necessary
+    # Perform cleanup when an entry is unloaded (e.g., unload entities)
     return True
-
