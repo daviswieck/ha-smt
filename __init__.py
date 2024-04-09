638282a8adc33474fd60f52706513e46e5aa8c1a
gopher_dave <79770840+daviswieck@users.noreply.github.com>
2024-04-08 21:29:22 -0500

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,1 +1,22 @@
+"""The SmartMeterTexas integration."""
+import logging
+
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
+
+DOMAIN = "smt"
+
+async def async_setup(hass: HomeAssistant, config: dict):
+    """Set up the SmartMeterTexas component."""
+    return True
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
+    """Set up SmartMeterTexas from a config entry."""
+    # Add setup code here, if necessary
+    return True
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
+    """Unload a config entry."""
+    # Add cleanup code here, if necessary
+    return True
 
